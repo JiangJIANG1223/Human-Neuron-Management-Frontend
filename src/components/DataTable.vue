95036eaa1b76bd32fa36432e07317fe29f10e78f
guowen-g <85502398+guowen-g@users.noreply.github.com>
2024-09-13 15:38:06 +0800

    Add files via upload

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -132,15 +132,37 @@
     <el-dialog title="View Data" v-model="viewDialogVisible" width="74%">
       <el-form ref="viewFormRef" :model="viewForm" label-width="240px" class="custom-dialog-content">
         <!-- 显示图像 -->
-        <div v-if="viewForm.image_file" class="image-container">
-          <img 
-            v-if="imageUrl" 
-            :src="imageUrl" 
-            alt="Image" class="image-preview" 
-            style="width: 12%;" 
-            @dblclick="showFullImage(viewForm.image_file)">
-          <div class="image-label">MIP</div>
-        </div>
+            <div style="display: flex; justify-content: center;">
+                <div style="display: flex; align-items: center; justify-content: center;">
+
+                    <!-- Always show MIP -->
+                    <div v-if="viewForm.image_file" class="image-container" style="margin: 3px;">
+                        <img :src="imageUrl" alt="mip Image" style="width: 70%;" class="image-preview" @dblclick="showFullImage(viewForm.image_file)"/>
+                        <div class="image-label">MIP</div>
+                    </div>
+
+                    <!-- Initially show Both -->
+                    <div class="image-container" style="margin: 3px;">
+                        <img v-show="currentImage === 'both'" :src="mip_swc_url" alt="Both Image" style="width: 70%;" class="image-preview" @dblclick="showFullBoth(viewForm.image_file, viewForm)"/>
+                        <img v-show="currentImage === 'swc'" :src="swcUrl" alt="SWC Image" style="width: 70%;" class="image-preview" @dblclick="showFullSWC(viewForm)"/>
+                        <div class="image-label">{{ currentImageLabel }}</div>
+
+                        <!-- View/Hide SWC Button -->
+                        <el-button type="primary" @click="toggleImage" style="position: absolute; top: 120px; right: 5px;">{{ buttonLabel }}</el-button>
+
+                        <!-- Dropdown -->
+                        <select v-model="selectedVersion" @change="updateSwcUrl" class="custom-select" style="position: absolute; top: 160px; right: 5px;">
+                            <option value="v0">Auto_1.4</option>
+                            <option value="v1">其它版本未上线1</option>
+                            <option value="v2">其它版本未上线2</option>
+                        </select>
+                    </div>
+
+
+
+                </div>
+            </div>
+
         <!-- 单独的 Cell ID 行 -->
         <el-row justify="center" class="cell-id-row">
           <el-col justify="center":span="7" class="cell-id-col">
@@ -167,9 +189,15 @@
       <div slot="footer" class="dialog-footer">
         <div class="left-buttons">
           <el-button v-if="isEdit" type="danger" @click="confirmDelete">删除</el-button>
+          <div>
+              <el-button type="primary" @click="triggerFileInput" style="margin-right: 10px;">Upload 3D Image</el-button>
+              <input type="file" ref="fileInput" @change="handleFileChange" style="display: none;" />
+          </div>
+	    <el-button type="primary" @click="select2DFolder">Upload 2D Image</el-button>
+            <input type="file" ref="fileInput2D" webkitdirectory multiple @change="handleFiles" style="display: none;" />
         </div>
         <div class="right-buttons">
-          <el-button @click="viewDialogVisible = false">取消</el-button>
+          <el-button @click="viewDialogVisible = false">取消</el-button>	  
           <el-button v-if="!isEdit" type="primary" @click="startEditing" :disabled="isGuest">编辑</el-button>
           <el-button v-if="isEdit" type="primary" @click="submitEdit">保存</el-button>
         </div>
@@ -235,6 +263,20 @@ export default {
   },
   data() {
     return {
+
+      selectedVersion: 'v0', // 默认选择
+      swcUrlv0:'',// 默认版本SWC图像的URL
+      swcUrlv1: '', // v1 版本的 URL
+      swcUrlv2: '', // v2 版本的 URL
+
+      mip_swc_urlv0:'', //展示默认版本的MIP—SWC图像用
+      mip_swc_urlv1:'', //展示MIP—SWC图像用
+      mip_swc_urlv2:'', //展示MIP—SWC图像用
+
+
+      currentImage: 'both', // 初始显示Both，MIP和Both交替展示用
+      folderPath: '', // 批量化转换用
+
       tableData: [],
       totalItems: 0,
       pageSize: 20,
@@ -248,6 +290,8 @@ export default {
       selectAllPages: false, 
       fullImageDialogVisible: false,
       imageUrl:'',
+      swcUrl:'',   //展示SWC图像用
+      mip_swc_url:'', //展示MIP—SWC图像用
       fullImageUrl: '',
       uploadLoading: false,
       downloadLoading: false,
@@ -367,10 +411,132 @@ export default {
       }
     }
   },
+  // 为MIP和Both交替可视化进行设置
+  computed: {
+        buttonLabel() {
+            return this.currentImage === 'both' ? 'View SWC' : 'View Both';
+        },
+        currentImageLabel() {
+            return this.currentImage === 'both' ? 'Both' : 'SWC';
+        },
+
+  },
   mounted() {
     this.fetchOptions();
   },
   methods: {
+  //**************************2D 图像上传拼接用***************************
+    select2DFolder() {
+      this.$refs.fileInput2D.click();
+    },
+
+    async handleFiles(event) {
+      const files = event.target.files;
+      const formData = new FormData();
+
+      // 获取文件夹名称，假设文件的路径格式为 "folderName/fileName.ext"
+      const folderName = files[0].webkitRelativePath.split('/')[0]; // 获取第一个文件的相对路径中的文件夹名称
+
+      // 将文件夹名称添加到 FormData
+      formData.append('folderName', folderName);
+
+      for (let file of files) {
+        if (file.name.endsWith('.tif') || file.name.endsWith('.png')) {
+          formData.append('files', file);
+        }
+      }
+
+      try {
+        const response = await fetch('/api/2Dupload/', {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          console.log('Upload successful:', result);
+          this.$message.success('上传成功');
+        } else {
+          console.error('Upload failed:', response.statusText);
+          this.$message.error('上传失败1');
+        }
+      } catch (error) {
+        console.error('Error uploading files:', error);
+        this.$message.error('上传失败2');
+      } finally {
+        // 重置 input 的值
+        this.$refs.fileInput.value = null;
+      }
+    },
+
+  //**************************2D 图像上传拼接用，结束**********************
+   //**********************多版本SWC展示用********************************
+    updateSwcUrl() {
+    if (this.selectedVersion === 'v0')
+    {
+      if(this.viewForm.swc_auto14)
+          {this.swcUrl = this.swcUrlv0;
+           this.mip_swc_url = this.mip_swc_urlv0;} // 使用默认的 SWC URL,swcUrl是最终用到的展示路径
+      else
+          {this.swcUrl = '';
+           this.mip_swc_url = '';}
+    }
+    else if (this.selectedVersion === 'v1')
+    {
+      if(this.viewForm.swc_v1)
+          {this.swcUrl = this.swcUrlv1;
+           this.mip_swc_url = this.mip_swc_urlv1;} // 使用默认的 SWC URL,swcUrl是最终用到的展示路径
+      else
+          {this.swcUrl='';
+           this.mip_swc_url = '';}
+
+    }
+     else if (this.selectedVersion === 'v2')
+    {
+      if(this.viewForm.swc_v2)
+          {this.swcUrl = this.swcUrlv2;
+           this.mip_swc_url = this.mip_swc_urlv2;} // 使用默认的 SWC URL,swcUrl是最终用到的展示路径
+      else
+          {this.swcUrl='';
+           this.mip_swc_url = '';}
+    }
+  },
+   //***********************MIP-SWC可视化********************************
+   toggleImage() {
+            this.currentImage = this.currentImage === 'both' ? 'swc' : 'both';
+     },
+   //*************************MIP-SWC可视化***************************
+//**************************单样本数据格式转换上传部分代码*************************************
+    triggerFileInput() {
+          this.$refs.fileInput.click();
+        },
+        async handleFileChange(event) {
+          const file = event.target.files[0];
+          if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+            alert(`${file}`)
+
+            try {
+              const response = await fetch('/api/singleConvert/', {
+                method: 'POST',
+                body: formData,
+              });
+              if (response.ok) {
+                alert('文件上传成功');
+              } else {
+                alert('文件上传失败');
+              }
+            } catch (error) {
+              console.error('Error during file upload:', error);
+              this.$message.error('文件转换失败');
+            }
+             finally {
+                // 重置 input 的值
+                this.$refs.fileInput.value = null;}
+          }
+        },
+     //**********************************单样本数据格式转换上传部分代码结束*************************************
     fetchData() {
       let that = this
       const params = {
@@ -540,9 +706,26 @@ export default {
       });
     },
     viewData(row) {
-      this.viewForm = { ...row };
-      this.isEdit = false;
-      this.viewDialogVisible = true;
+
+        this.viewForm = { ...row };
+        this.selectedVersion = 'v0';
+        this.currentImage = 'both';
+
+        // 先获取 URL
+        Promise.all([
+            this.getImageUrl2(row.image_file, row.swc_auto14).then(url => {
+                this.mip_swc_url = url;
+            }),
+            this.getImageUrl1(row.swc_auto14).then(url => {
+                this.swcUrl = url;
+            })
+        ]).catch(error => {
+            console.error('Error fetching images:', error);
+        });
+
+
+        this.isEdit = false;
+        this.viewDialogVisible = true;
     },
 
     // getImageUrl(imagePath) {
@@ -559,16 +742,118 @@ export default {
         return '';
       }
     },
+    async getImageUrl1(ss) {  //获取SWC图像的,ss是传入的不同版本的swc文件地址
+
+    if(!ss){
+        this.$message.error('No SWC file');
+        return '';
+
+    }
+
+      try {
+       const response = await axios.post('/api/getSWC/', {
+                   ss:ss  // 将文件夹路径发送到后端
+                  }, {
+                     responseType: 'blob' // 这里设置为 blob
+                   });
+        const swcUrl = URL.createObjectURL(response.data);
+        return swcUrl;
+      } catch (error) {
+        console.error('Error fetching image:', error);
+        this.$message.error('获取图像失败1');
+        return '';
+      }
+    },
+    async getImageUrl2(imageFile, swcFile) { //获取MIP和SWC图像叠加图的
+      if(!imageFile || !swcFile)
+      {
+            return '';
+      }
+
+      try {
+        const response = await axios.post('/api/getMIPSWC/', {
+          image_file: imageFile,  // 将image_file发送到后端
+          swc_file: swcFile       // 将swc_auto14发送到后端
+        }, {
+          responseType: 'blob' //
+        });
+
+        const mip_swc_url = URL.createObjectURL(response.data);
+        return mip_swc_url;  // 返回生成的URL
+
+      } catch (error) {
+        console.error('Error fetching combined image:', error);
+        this.$message.error('获取图像失败2');
+        return '';
+      }
+    },
     async showImage() {
       if (this.viewForm.image_file) {
-        this.imageUrl = await this.getImageUrl(this.viewForm.image_file);
+            this.imageUrl = await this.getImageUrl(this.viewForm.image_file);
       }
+
+      if (this.viewForm.swc_auto14) {
+            this.swcUrlv0 = await this.getImageUrl1(this.viewForm.swc_auto14);
+      }
+      if (this.viewForm.swc_v1) {
+            this.swcUrlv1 = await this.getImageUrl1(this.viewForm.swc_v1);
+      }
+      if (this.viewForm.swc_v2) {
+            this.swcUrlv2 = await this.getImageUrl1(this.viewForm.swc_v2);
+      }
+
+      if (this.viewForm.image_file && this.viewForm.swc_auto14) {
+            this.mip_swc_urlv0=await this.getImageUrl2(this.viewForm.image_file, this.viewForm.swc_auto14);
+      }
+      if (this.viewForm.image_file && this.viewForm.swc_v1) {
+            this.mip_swc_urlv1=await this.getImageUrl2(this.viewForm.image_file, this.viewForm.swc_v1);
+      }
+      if (this.viewForm.image_file && this.viewForm.swc_v2) {
+            this.mip_swc_urlv2=await this.getImageUrl2(this.viewForm.image_file, this.viewForm.swc_v2);
+      }
+
     },
     
     async showFullImage(imagePath) {
       this.fullImageUrl = await this.getImageUrl(imagePath);
       this.fullImageDialogVisible = true;
     },
+    async showFullSWC(imagePath) {    //展示不同版本SWC图像
+    if(this.selectedVersion === 'v0')
+     {
+          this.fullImageUrl = await this.getImageUrl1(imagePath.swc_auto14);
+          this.fullImageDialogVisible = true;
+     }
+     else if (this.selectedVersion === 'v1')
+     {
+          this.fullImageUrl = await this.getImageUrl1(imagePath.swc_v1);
+          this.fullImageDialogVisible = true;
+     }
+     else if (this.selectedVersion === 'v2')
+     {
+          this.fullImageUrl = await this.getImageUrl1(imagePath.swc_v2);
+          this.fullImageDialogVisible = true;
+     }
+    },
+    async showFullBoth(imagePath,swcPath) {
+        if(this.selectedVersion === 'v0')
+         {
+              this.fullImageUrl = await this.getImageUrl2(imagePath,swcPath.swc_auto14);
+              this.fullImageDialogVisible = true;
+         }
+         else if (this.selectedVersion === 'v1')
+         {
+              this.fullImageUrl = await this.getImageUrl2(imagePath,swcPath.swc_v1);
+              this.fullImageDialogVisible = true;
+         }
+         else if (this.selectedVersion === 'v2')
+         {
+              this.fullImageUrl = await this.getImageUrl2(imagePath,swcPath.swc_v2);
+              this.fullImageDialogVisible = true;
+         }
+
+    },
+
     startEditing() {
       this.isEdit = true;
     },
@@ -698,6 +983,10 @@ export default {
     },
     getEmptyForm() {
       return {
+        swc_v2:'',
+        swc_v1:'',
+        swc_auto14:'',
+        image_file: '',
         cell_id: '',
         patient_number: '',
         tissue_block_number: '',
@@ -771,7 +1060,9 @@ export default {
     },
     handlePictureCardPreview(file) {
       // 使用动态的 axios 默认路径
-      this.previewImageUrl = `${axios.defaults.baseURL}/Record_Book_Pics/${file.name}`;
+      const timestamp = new Date().getTime(); // 获取当前时间戳
+      // this.previewImageUrl = `${axios.defaults.baseURL}/Record_Book_Pics/${file.name}`;
+      this.previewImageUrl = `${axios.defaults.baseURL}/Record_Book_Pics/${file.name}?v=${timestamp}`;
       this.previewDialogVisible = true;
     },
     beforeUpload(file) {
@@ -817,7 +1108,10 @@ export default {
           this.$message.error('Failed to fetch record book pics');
         });
     }
-  }
+  },
+    mounted() {
+        this.updateSwcUrl(); // 初始化时更新不同版本 SWC URL
+      }
 };
 </script>
 
@@ -942,6 +1236,28 @@ export default {
   margin-top: 5px;
   font-weight: bold;
 }
+/* 多版本SWC下拉框style设置*/
+.custom-select {
+    width: 100px; /* 设置初始宽度 */
+    padding: 8px; /* 内边距 */
+    border: 1px solid #ccc; /* 边框 */
+    border-radius: 5px; /* 圆角 */
+    background-color: #fff; /* 背景颜色 */
+    color: #333; /* 字体颜色 */
+    font-size: 16px; /* 字体大小 */
+    cursor: pointer; /* 鼠标悬停时显示为手形 */
+    transition: border-color 0.3s; /* 添加过渡效果 */
+}
+
+.custom-select:hover {
+    border-color: #007BFF; /* 悬停时改变边框颜色 */
+}
+
+.custom-select:focus {
+    outline: none; /* 去掉默认的聚焦轮廓 */
+    border-color: #007BFF; /* 聚焦时改变边框颜色 */
+}
+/* 多版本SWC下拉框style设置结束 */
 /* .full-image-preview {
   width: 100%;
 } */
