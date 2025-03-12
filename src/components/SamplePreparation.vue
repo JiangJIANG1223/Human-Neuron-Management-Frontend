581ba5bbb7c45042717b77bf0ac30f0dd00726cb
WangLJ-SEU <32135169+PrometheusTT@users.noreply.github.com>
2025-03-12 09:27:02 +0800

    Update SamplePreparation.vue

--- src/components/SamplePreparation.vue
+++ src/components/SamplePreparation.vue
@@ -363,8 +363,8 @@
               <el-button type="primary" class="btn" @click="downloadImagingFiles(img)" :disabled="isGuest">Download</el-button>
             </td>
             <td>
-              <el-button type="primary" class="btn" @click="preview(img)">Preview</el-button>
-              <el-button type="primary" class="btn" @click="toCell(img)">Submit</el-button>
+              <el-button type="primary" class="btn" @click="preview(img)" :disabled="!(img.status === 'marked')">Preview</el-button>
+              <el-button type="primary" class="btn" @click="toCell(img)" :disabled="!(img.status === 'marked')">Submit</el-button>
               <!--              <el-button type="primary" class="btn" @click="toCell(img)" :disabled="!(img.status === 'marked'|| img.status === 'inserted')">Submit</el-button>-->
             </td>
           </tr>
